be03c86d4d9fd44740fb0788ed39a5af7d7f4286
Prakash Raman <prakash.raman@sentiance.com>
2024-01-10 17:21:58 -0500

    Removed unwanted comment

--- lib/src/helpers/auth.dart
+++ lib/src/helpers/auth.dart
@@ -13,11 +13,9 @@ Future<AuthCodeResult> fetchAuthCode() async {
   final response = await http.get(Uri.parse('http://localhost:8000/auth/code'));
 
   if (response.statusCode == 200) {
-    // If the server returns a 200 OK response, parse the JSON.
     final Map<String, dynamic> data = json.decode(response.body);
     return AuthCodeResult(data['auth_code'], data['app_id']);
   } else {
-    // If the server returns an unsuccessful response code, throw an exception.
     throw Exception('Failed to load data');
   }
 }
