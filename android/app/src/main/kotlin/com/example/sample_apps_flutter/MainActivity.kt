cdd291b2b92393ee20a1d81e0372e4a5bc8ec24d
Prakash Raman <prakash.raman@sentiance.com>
2024-02-24 18:07:29 -0500

    Created MainApplication.kt

--- android/app/src/main/kotlin/com/example/sample_apps_flutter/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/sample_apps_flutter/MainActivity.kt
@@ -1,6 +1,16 @@
 package com.example.sample_apps_flutter
 
-import io.flutter.embedding.android.FlutterActivity
+import com.sentiance.sentiance_plugin.SentianceHelper
+import io.flutter.app.FlutterApplication
 
-class MainActivity: FlutterActivity() {
+class MainApplication : FlutterApplication() {
+
+    private lateinit var sentianceHelper: SentianceHelper
+
+    override fun onCreate() {
+        super.onCreate()
+        sentianceHelper = SentianceHelper.getInstance(this);
+        sentianceHelper.initialize();
+    }
+
 }
